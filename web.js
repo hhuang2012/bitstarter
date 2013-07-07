b15c67760d4573d7419b41a297c044e091e112f1
Ubuntu <ubuntu@ip-172-31-16-153.us-west-2.compute.internal>
2013-07-07 15:44:14 +0000

    read html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var data = fs.readFileSync('home/ubuntu/bitstarter/index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send(data);
 });
 
 var port = process.env.PORT || 5000;
